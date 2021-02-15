f6f80a5ac251db3e25f21ee8450fba662082829d
Abdul Uddin <auddin431@nbp-200-150.nbp.ruw.rutgers.edu>
2021-02-14 20:56:04 -0500

    updated navbar

--- integrated-cinema-automation/src/pages/NavBar.js
+++ integrated-cinema-automation/src/pages/NavBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import "../App.css";
 
 const NavBar = () => {
   return (
@@ -9,23 +10,14 @@ const NavBar = () => {
       <Navbar.Brand href="#home">Integrated Cinema Automation</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#features">Tickets</Nav.Link>
-          <Nav.Link href="#pricing">Food</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">My Account</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Data Analytics
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Scan My Code</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Not Sure</NavDropdown.Item>
-          </NavDropdown>
+        <Nav className="mr-auto ml-auto">
+          <Nav.Link href="#movies">Movies</Nav.Link>
+          <Nav.Link href="#menu">Dine-In Menu</Nav.Link>
+          <Nav.Link href="#tickets">My Tickets</Nav.Link>
         </Nav>
         <Nav>
-          <Nav.Link href="#deets">Log In</Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">
-            Register
+          <Nav.Link eventKey={2} href="#account">
+            My Account
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
