177eae5fd9dab0667a9ddc6b1937318d4e06c959
John Reiner Sloan <johnreiner307@gmail.com>
2021-02-18 13:02:44 -0500

    changes to MovieSelection. still broke

--- integrated-cinema-automation/src/components/pages/MovieSelection.js
+++ integrated-cinema-automation/src/components/pages/MovieSelection.js
@@ -1,16 +1,15 @@
 import React from "react";
 import NavBar from "./NavBar";
 import "../../App.css";
+import MovieCards from "../MovieCards";
+import './MovieSelection.css';
 
 const MovieSelection = () => {
     return (
         <>
-            <NavBar />
-            <div className="App">
-                <header className="App-header">
-                    <h1>Movie UwU</h1>
-                    <p>This is the home page</p>
-                </header>
+            <div className="MovieSelection">
+                <NavBar />
+                <MovieCards />
             </div>
         </>
     );
