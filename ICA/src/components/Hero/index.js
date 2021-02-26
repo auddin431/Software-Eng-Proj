335486f0b5de385219f6ff0deed58cb41d246d9b
Abdul Uddin <auddin431@nbp-221-190.nbp.ruw.rutgers.edu>
2021-02-26 18:40:44 -0500

    added backend to food ordering

--- ICA/src/components/Hero/index.js
+++ ICA/src/components/Hero/index.js
@@ -1,30 +1,47 @@
 //type rafce + tab to get basic structure
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import NavBar from "../pages/NavBar";
-import Sidebar from '../Sidebar';
-import { HeroContainer, HeroContent, HeroItems, HeroH1, HeroP, HeroBtn } from './HeroElements';
+import Sidebar from "../Sidebar";
+import {
+  HeroContainer,
+  HeroContent,
+  HeroItems,
+  HeroH1,
+  HeroP,
+  HeroBtn,
+} from "./HeroElements";
 
+export default function Hero() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [test, setTest] = useState("");
 
-export default function Hero(){
- const [isOpen, setIsOpen] = useState(false)
- 
- const toggle = () => {
-     setIsOpen(!isOpen)
- }
+  const call = async () => {
+    try {
+      const ret = await fetch("http://localhost:9000/testAPI");
+      const text = await ret.text();
+      setTest(text);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    return (
-       <HeroContainer>
-           <NavBar />
-           <Sidebar isOpen={isOpen} toggle={toggle} />
-           <HeroContent>
-               <HeroItems>
-                   <HeroH1>Greatest Hotdog Ever</HeroH1>
-                   <HeroP>Ready in 60 seconds</HeroP>
-                   <HeroBtn  onClick={toggle}>Open Menu</HeroBtn>
-               </HeroItems>
-           </HeroContent>
-       </HeroContainer>
-    );
-};
+  const toggle = () => {
+    setIsOpen(!isOpen);
+    call();
+  };
 
-
+  return (
+    <HeroContainer>
+      <NavBar />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <HeroContent>
+        <HeroItems>
+          <HeroH1>Greatest Hotdog Ever</HeroH1>
+          <HeroP>Ready in 60 seconds</HeroP>
+          <HeroP>{test}</HeroP>
+          <HeroBtn onClick={toggle}>Open Menu</HeroBtn>
+        </HeroItems>
+      </HeroContent>
+    </HeroContainer>
+  );
+}
