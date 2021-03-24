f68f8a717052eea6fdd811a8278dbbcf90bd2a26
acats525 <aac250@scarletmail.rutgers.edu>
2021-03-23 21:53:46 -0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ var cors = require("cors");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var testAPIRouter = require("./routes/testAPI");
-var testPostRouter = require("/routes/testPost");
+var testPostRouter = require("./routes/testPost");
 
 var app = express();
 
