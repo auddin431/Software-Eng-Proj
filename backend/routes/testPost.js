4a35140017ba08d3003911c7c89dd2a5ef2acf75
Abdul Uddin <auddin431@MacBook-Pro.fios-router.home>
2021-03-23 21:54:01 -0400

    developed food order schema and connected to local mongo service

--- backend/routes/testPost.js
+++ backend/routes/testPost.js
@@ -1,9 +1,28 @@
 var express = require("express");
+const mongoose = require("mongoose");
+const url = "mongodb://127.0.0.1:27017/food_order";
 var router = express.Router();
 
-router.post("/", function (req, res, next) {
-  res.send("API is working properly");
-  console.log("Hell Bitch");
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+const foodSchema = new mongoose.Schema(
+  {
+    name: String,
+    price: String,
+  },
+  { collection: "orders" }
+);
+const Order = mongoose.model("Order", foodSchema, "orders");
+
+router.post("/", function (req, res, next) {
+  console.log(req.body.name);
+  console.log(req.body.price);
+  const order = new Order({ name: req.body.name, price: req.body.price });
+  order.save(function (err) {
+    if (err) return console.error(err);
+  });
+});
 module.exports = router;
