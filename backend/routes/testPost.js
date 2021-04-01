d6553467a921404c3a26899e8830137070376d31
unknown <drago7654321@gmail.com>
2021-04-01 09:28:23 -0400

    Fixed testPost.js

--- backend/routes/testPost.js
+++ backend/routes/testPost.js
@@ -1,33 +1,33 @@
-// var express = require("express");
-// const mongoose = require("mongoose");
-// const url = "mongodb://127.0.0.1:27017/food_order";
-// var router = express.Router();
+var express = require("express");
+const mongoose = require("mongoose");
+const url = "mongodb://127.0.0.1:27017/food_order";
+var router = express.Router();
 
-// mongoose.connect(url, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// const foodSchema = new mongoose.Schema(
-//   {
-//     name: String,
-//     price: String,
-//   },
-//   { collection: "orders" }
-// );
-// const Order = mongoose.model("Order", foodSchema, "orders");
+const foodSchema = new mongoose.Schema(
+  {
+    name: String,
+    price: String,
+  },
+  { collection: "orders" }
+);
+const Order = mongoose.model("Order", foodSchema, "orders");
 
-// router.post("/", function (req, res, next) {
-//   console.log(req.body.name);
-//   console.log(req.body.price);
-//   const order = new Order({ name: req.body.name, price: req.body.price });
-//   order.save(function (err) {
-//     if (err) return console.error(err);
-//     console.log("Order was saved");
-//   });
-//   Order.find({}, (err, orders) => {
-//     if (err) return console.error(err);
-//     console.log(orders);
-//   });
-// });
-// module.exports = router;
+router.post("/", function (req, res, next) {
+  console.log(req.body.name);
+  console.log(req.body.price);
+  const order = new Order({ name: req.body.name, price: req.body.price });
+  order.save(function (err) {
+    if (err) return console.error(err);
+    console.log("Order was saved");
+  });
+  Order.find({}, (err, orders) => {
+    if (err) return console.error(err);
+    console.log(orders);
+  });
+});
+module.exports = router;
