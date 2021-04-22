d01a137bd6e7a140cd194cd555b499d33985a723
Abdul Uddin <auddin431@gmail.com>
2021-04-21 21:17:39 -0400

    rendered cart items from database

--- ica/src/components/pages/Checkout/Edit.js
+++ ica/src/components/pages/Checkout/Edit.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 
 const products = [
-  { name: 'Avengers: Endgame', desc: 'One ticket', price: '$12.00' },
-  { name: 'Appetizer: Ultimate Nachos', desc: 'Crispy tortilla chips, topped with seasoned chicken, cheese, Pico de Gallo, and other favorite nacho toppings', price: '$10.99' },
-  { name: 'Entree: Spaghetti & Meatballs', desc: 'Perfectly cooked spaghetti topped with tangy tomato sauce pairs with our seasoned Italian meatballs', price: '$11.99' },
-  { name: 'Drink: Coca-Cola', desc: 'Classic Coke flavor that is refreshing', price: '$2.50' },
-
+  { name: "Avengers: Endgame", desc: "One ticket", price: "$12.00" },
+  {
+    name: "Appetizer: Ultimate Nachos",
+    desc:
+      "Crispy tortilla chips, topped with seasoned chicken, cheese, Pico de Gallo, and other favorite nacho toppings",
+    price: "$10.99",
+  },
+  {
+    name: "Entree: Spaghetti & Meatballs",
+    desc:
+      "Perfectly cooked spaghetti topped with tangy tomato sauce pairs with our seasoned Italian meatballs",
+    price: "$11.99",
+  },
+  {
+    name: "Drink: Coca-Cola",
+    desc: "Classic Coke flavor that is refreshing",
+    price: "$2.50",
+  },
 ];
 
-const addresses = ['1 Rutgers Way', 'Rutgersville', 'Rutgers', '12345', 'USA'];
+const addresses = ["1 Rutgers Way", "Rutgersville", "Rutgers", "12345", "USA"];
 const payments = [
-  { name: 'Card type', detail: 'Doge' },
-  { name: 'Card holder', detail: 'John Reiner Sloan' },
-  { name: 'Card number', detail: 'xxxx-xxxx-xxxx-1234' },
-  { name: 'Expiry date', detail: '06/1234' },
+  { name: "Card type", detail: "Doge" },
+  { name: "Card holder", detail: "John Reiner Sloan" },
+  { name: "Card number", detail: "xxxx-xxxx-xxxx-1234" },
+  { name: "Expiry date", detail: "06/1234" },
 ];
 
 const useStyles = makeStyles((theme) => ({
@@ -37,6 +50,12 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Edit(props) {
   const classes = useStyles();
+  var price = 0;
+  props.products.map((product) => {
+    var yes = parseFloat(product.price.slice(1));
+    price += yes;
+  });
+  price = "$" + price;
   console.log(props.products);
   return (
     <React.Fragment>
@@ -44,25 +63,23 @@ export default function Edit(props) {
         Order summary
       </Typography>
       <List disablePadding>
-        {props.products.map((product) => (
-          <ListItem className={classes.listItem} key={product.name}>
-            <ListItemText primary={product.name} />
-            <Typography variant="body2">{product.price}</Typography>
-            <Button className={classes.button}>+</Button>
-            <Button className={classes.button}>-</Button>
-          </ListItem>
-          
-        ))}
+        {props.products &&
+          props.products.map((product) => (
+            <ListItem className={classes.listItem} key={product._id}>
+              <ListItemText primary={product.name} />
+              <Typography variant="body2">{product.price}</Typography>
+              <Button className={classes.button}>+</Button>
+              <Button className={classes.button}>-</Button>
+            </ListItem>
+          ))}
         <ListItem className={classes.listItem}>
           <ListItemText primary="Total" />
           <Typography variant="subtitle1" className={classes.total}>
-            $37.48
+            {price}
           </Typography>
         </ListItem>
       </List>
-      <Grid container spacing={2}>
-        
-      </Grid>
+      <Grid container spacing={2}></Grid>
     </React.Fragment>
   );
 }
